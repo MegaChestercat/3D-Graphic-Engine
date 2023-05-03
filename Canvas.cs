9421a6c92c70cc7ee6c6455569d46873b01477b7
Omar-Atriano <omar.atrianova@udlap.mx>
2023-05-03 01:21:51 -0600

    Save over OBJ

--- Canvas.cs
+++ Canvas.cs
@@ -39,6 +39,32 @@ namespace Optimized_3D_Graphic_Engine
         {
             Init(s.Width, s.Height);
         }
+        Vertex[] vertices = new Vertex[] {
+                                            new Vertex(1, 1, 1),
+                                            new Vertex(-1, 1, 1),
+                                            new Vertex(-1, -1, 1),
+                                            new Vertex(1, -1, 1),
+                                            new Vertex(1, 1, -1),
+                                            new Vertex(-1, 1, -1),
+                                            new Vertex(-1, -1, -1),
+                                            new Vertex(1, -1, -1)
+                                        };
+
+
+        Triangle[] triangulos = new Triangle[] {
+                                            new Triangle(0, 1, 2, Color.Red),
+                                            new Triangle(0, 2, 3, Color.Red),
+                                            new Triangle(4, 0, 3, Color.Green),
+                                            new Triangle(4, 3, 7, Color.Green),
+                                            new Triangle(5, 4, 7, Color.Blue),
+                                            new Triangle(5, 7, 6, Color.Blue),
+                                            new Triangle(1, 5, 6, Color.Yellow),
+                                            new Triangle(1, 6, 2, Color.Yellow),
+                                            new Triangle(4, 5, 1, Color.Purple),
+                                            new Triangle(4, 1, 0, Color.Purple),
+                                            new Triangle(2, 6, 7, Color.Cyan),
+                                            new Triangle(2, 7, 3, Color.Cyan)
+                                           };
 
         public void Init(int width, int height)
         {
@@ -75,10 +101,10 @@ namespace Optimized_3D_Graphic_Engine
                 }
             }
             models = new List<Model>();
-            //models.Add(new Model(vertices, triangulos, new Vertex(0, 0, 0), (float)Math.Sqrt(3)));
+            models.Add(new Model(vertices, triangulos, new Vertex(0, 0, 0), (float)Math.Sqrt(3)));
 
-            instances = new List<Instance> ();
-            //{    new Instance(models[0], new Vertex(     0,     0, 7.5f ), Matrix.RotY(0))};
+            instances = new List<Instance> {
+                 new Instance(models[0], new Vertex(     0,     0, 7.5f ), Matrix.RotY(0))};
             ClippingPlanes(90);
         }
 
